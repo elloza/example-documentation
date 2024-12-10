947939dc0dfa6627bf764dfb79ac6f2094ce7066
UsuarioAula <aula@usal.es>
2024-12-10 18:58:32 +0100

    feat: add documentation

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+/***
+ * 
+ * This is the main class of the program
+ * 
+ * @author Alvaro
+ * @version 2.2
+ * 
+ */
+public class Main {
+    /**
+     * 
+     * The main method of the app
+     * 
+     * @param args the command line arguments for the app
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Gson gs = new Gson();
+
+    }
+}
